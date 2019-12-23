b604835832727de4d8f4eb3513feaa1fc1e79948
juanborbon93 <39175900+juanborbon93@users.noreply.github.com>
2019-12-22 22:08:35 -0600

    changed tp accept db url

--- db.py
+++ db.py
@@ -8,5 +8,6 @@ host=os.environ['HOST']
 port=os.environ['PORT']
 database=os.environ['DATABASE']
 
-db.bind(provider='postgres', user=username, password=password, host=host ,port=port, database=database)
+# db.bind(provider='postgres', user=username, password=password, host=host ,port=port, database=database)
+db.bind(provider='postgres', dsn=os.environ['DATABASE_URL'])
 db.generate_mapping(create_tables=True)
