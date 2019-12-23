55786703581b513331e3c09082615c35907966fe
juanborbon93 <39175900+juanborbon93@users.noreply.github.com>
2019-12-22 22:09:15 -0600

    removed redundant env variables

--- db.py
+++ db.py
@@ -2,11 +2,11 @@ from modules.database import *
 import os
 
 
-username=os.environ['USER']
-password=os.environ['PASSWORD']
-host=os.environ['HOST']
-port=os.environ['PORT']
-database=os.environ['DATABASE']
+# username=os.environ['USER']
+# password=os.environ['PASSWORD']
+# host=os.environ['HOST']
+# port=os.environ['PORT']
+# database=os.environ['DATABASE']
 
 # db.bind(provider='postgres', user=username, password=password, host=host ,port=port, database=database)
 db.bind(provider='postgres', dsn=os.environ['DATABASE_URL'])
